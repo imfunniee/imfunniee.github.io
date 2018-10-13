00ca58ae482cd5b5da84e0df8c21117c1ffb9bfe
imfunny <36105478+imfunniee@users.noreply.github.com>
2018-10-13 15:38:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -66,3 +66,13 @@ function showweb(){
     $("#web-btn").css("border-bottom","4px solid #000");
     $("#des-btn").css("border-bottom","4px solid transparent");
 }
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.body.style.backgroundSize = "100% 100%";
+    }else{
+        document.body.style.backgroundSize = "140% 140%";
+    }
+}
