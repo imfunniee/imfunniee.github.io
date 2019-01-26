25fa27ad4948992b68a6e75b52f76629c37d7103
imfunny <36105478+imfunniee@users.noreply.github.com>
2019-01-26 15:14:06 +0530

    Add files via upload

--- grip/index.js
+++ grip/index.js
@@ -1,7 +1,11 @@
 window.onscroll = function(){
-    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    if(document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
         document.body.style.backgroundSize = "150% auto";
+        $("#header").css("background","#000");
+        $("#header").css("padding","4vh 5vw");
     }else{
         document.body.style.backgroundSize = "100% auto";
+        $("#header").css("background","transparent");
+        $("#header").css("padding","4vh 5vw");
     }
 };
