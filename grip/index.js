c06f1870082b1f61d229986915d5c9f5b858f31a
imfunny <36105478+imfunniee@users.noreply.github.com>
2019-01-26 14:46:32 +0530

    Add files via upload

--- grip/index.js
+++ grip/index.js
@@ -1,1 +1,7 @@
-ok
+window.onscroll = function(){
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.body.style.backgroundSize = "150% auto";
+    }else{
+        document.body.style.backgroundSize = "100% auto";
+    }
+};
