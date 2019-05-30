13c0c8efa4c6be7eebc618066a33fb04c669dc06
imfunny <36105478+imfunniee@users.noreply.github.com>
2019-05-30 22:34:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var db = firebase.firestore();
+
 function showdes(){
     $("#web").css("display","none");
     $("#web-btn").css("border-bottom","3px solid transparent");
@@ -29,12 +31,65 @@ function takeitback(){
     },800);
 }
 
+$("#btn1").click(function(){
+    $(this).attr("disable",true);
+    var email = $("#email").val();
+    if(!email){
+        alert("Email can't be empty");
+        return;
+    }
+    $(this).html("please wait...");
+    db.collection("email").doc(email).set({
+        email: email,
+        time: new Date().getTime()
+    }).then(function(){
+        $("#mailing").html("<h2>Thank You for Joining 🎉</h2>");
+        $("#mailing").css("padding-bottom","2.5vh");
+        $("#mailing").css("background",'url("https://i.gifer.com/6k2.gif"),linear-gradient(0deg, rgba(0,0,0,0.4), rgba(0,0,0,0.4)),url("https://images.unsplash.com/photo-1550684656-96eda9e5c632?&w=1634") center center');
+        $(this).attr("disable",false);
+        setTimeout(function(){
+            $("#mailing").addClass("animated fadeOutRight");
+            setTimeout(function(){
+                $("#mailing").css("display", "none");
+                $("#mailing").removeClass("animated fadeOutRight");
+            },800)
+        },4000)
+    }).catch((err) => {
+        alert(err.message);
+        $(this).attr("disable",false);
+        $(this).html("Join");
+    });
+});
+
+$("#btn2").click(function(){
+    $("#mailing").addClass("animated fadeOutRight");
+    setTimeout(function(){
+        $("#mailing").css("display", "none");
+        $("#mailing").removeClass("animated fadeOutRight");
+    },800)
+});
+
+$("#btn2").mouseover(function(){
+    $(this).html("Go away 😦")
+});
+
+$("#btn2").mouseout(function(){
+    $(this).html("Go away")
+});
+
 setTimeout(function(){
     $("#text").addClass("animated fadeOut");
     setTimeout(function(){
         $("#text").removeClass("animated fadeOut");
         $("#text").css("display", "none");
     },800);
+    setTimeout(function(){
+        $("#mailing").css("display", "inline-block");
+        $("#mailing").addClass("animated fadeInRight");
+        setTimeout(function(){
+            $("#mailing").removeClass("animated fadeInRight");
+        },800);
+    },20000);
 },3000);
 
 setTimeout(function(){
